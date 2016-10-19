268e90a55899b4d603bedabe491c19498f2e0899
jpbeaudet <jpbeaudet84@gmail.com>
2016-10-19 11:56:41 -0400

    initial commit for boiler plate app

---
+++ config.js
@@ -0,0 +1,13 @@
+// Author: Jean-Philippe Beaudet @ S3R3NITY Technology 
+//
+// AppTemplate
+// Version : 0.0.1
+// License: 
+//
+var Config = {
+    node_web_server_host: "localhost",
+    node_web_server_port: 8080,
+    mongodb_database: 'template',
+    mongodb_host : "localhost"
+};
+module.exports = Config;
---
+++ routes/index.js
@@ -0,0 +1,26 @@
+// Author: Jean-Philippe Beaudet @ S3R3NITY Technology 
+//
+// AppTemplate
+// Version : 0.0.1
+// License: 
+//
+
+var mongoose = require('mongoose');
+
+
+exports.index = function (req, res) {
+	var username = "Not logged in";
+	var isAlreadyLoggedin = false;
+	// if the user is logged in 
+    if(req.user) {
+    	username = req.user.username;
+    	isAlreadyLoggedin = true;
+    }
+    var data = {
+        title: " AppTemplate",
+        username: username,
+        isAlreadyLoggedin:isAlreadyLoggedin
+    };
+    res.render('index/index', data);
+
+};
